30dcc022c84ddfcdf5c6de5c0ba72d7645cda222
madhumitha3147 <pamminajahnavi@gmail.com>
2025-05-22 12:34:15 +0530

    demo page audio functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,22 @@ export default {
       fontFamily: {
         overpass: ["'Overpass'", "sans-serif"],
       },
+      animation: {
+        'ping-slow': 'ping 2s cubic-bezier(0, 0, 0.2, 1) infinite',
+        'pingCustom': 'pingCustom 2.5s cubic-bezier(0, 0, 0.2, 1) infinite',
+      },
+      keyframes: {
+        pingCustom: {
+          '0%': {
+            transform: 'scale(1)',
+            opacity: '0.6',
+          },
+          '75%, 100%': {
+            transform: 'scale(1.2)',
+            opacity: '0',
+          },
+        },
+      },
     },
   },
   plugins: [],
