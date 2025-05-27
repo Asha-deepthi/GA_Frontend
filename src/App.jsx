211fae1d18e7b8690d3044042124b763b1a317b6
madhumitha3147 <pamminajahnavi@gmail.com>
2025-05-27 19:20:31 +0530

    sectionpage

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { StreamProvider } from './Test_execution/StreamContext';
-
+import SectionPage from "./Test_execution/SectionPage";
 import InstructionScreen from './Test_execution/InstructionScreen';
 import TutorialScreen from './Test_execution/TutorialScreen';
 import ConnectionStrength from './Test_execution/ConnectionStrength';
@@ -35,6 +35,7 @@ function App() {
           <Route path="/audioquestion" element={<AudioQuestion />} />
           <Route path="/demoquestion" element={<DemoQuestion />} />
           <Route path="/result" element={<ResultScreen />} />
+          <Route path="/sectionpage" element={<SectionPage />} />
         </Routes>
       </Router>
     </StreamProvider>
