61c56653394dd06dac00a8ad511ea510f27e89e0
unknown <joshikaboddeda@gmail.com>
2025-06-11 11:31:54 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import QuizCreationFlow from './Test_creation/QuizCreationFlow';
-import './App.css'; // Your new CSS file
+import TestSummaryScreen from './Test_creation/testSummaryScreen';
+// Your new CSS file
 
 function App() {
   return (
     <div className="App">
-      <QuizCreationFlow />
+      <TestSummaryScreen/>
     </div>
   );
 }
